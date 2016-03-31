29f4f270ba191cc065fb86ef5ab4825dba1a5c4a
Cldiaz <cindy.liliana.diaz@hotmail.com>
2016-03-31 18:44:56 -0400

    Initial Commit

---
+++ server/models/ticket.ts
@@ -0,0 +1,60 @@
+import mongoose = require('mongoose');
+
+// Define object schema
+var ticketSchema = new mongoose.Schema({
+    createdOn: {
+        type: Date,
+        default: Date.now
+    },
+    ticketNumber: {
+        type: String,
+        default: Date.now  
+    },
+    ticketTitle: {
+        type : String,
+        default: '',
+        trim: true,
+        required: 'Ticket Title is required'
+    },
+    ticketDescription : {
+        type : String,
+        default: '',
+        trim: true,
+        required: 'Ticket Description is required'
+    },
+    ticketPriority : {
+        type: String,
+        enum : ['HIGH', 'MEDIUM', 'LOW'],
+        default : 'LOW',
+        trim: true,
+        required: 'Ticket Priority is required'
+    },
+    customerName: {
+        type : String,
+        default: '',
+        trim: true,
+        required: 'Customer Name is required'
+    },
+    customerPhone: {
+        type : String,
+        default: '',
+        trim: true,
+        required: 'Customer Phone is required'
+    },
+    incidentNarrative : [{
+        commentDate : {
+            type: Date,
+            default: Date.now
+            },
+        comment: String,
+        ticketStatus : {
+            type: String,
+            enum : ['NEW', 'IN PROGRESS', 'CLOSED'],
+            default : 'NEW',
+            trim: true,
+            required: 'Ticket Status is required'
+        }
+    }]
+});
+
+export var Ticket = mongoose.model('Ticket', ticketSchema);
---
+++ server/routes/tickets.ts
@@ -0,0 +1,124 @@
+import express = require('express');
+var router = express.Router();
+
+//db references
+import mongoose = require('mongoose');
+
+import ticketModel = require('../models/ticket');
+
+import Ticket = ticketModel.Ticket;
+
+//utility function to check if user is authenticated
+function requireAuth(req: express.Request, res: express.Response, next: any){
+    //check if user is log in
+    if(!req.isAuthenticated()){
+        return res.redirect('/login');
+    }
+    next();
+}
+//get tickets page
+router.get('/', requireAuth,(req:express.Request, res:express.Response, next: any) =>{
+    var typeUser = req.user ? req.user.type : '';
+    if( typeUser == 'Admin'){
+        //use the Ticket model to query the Tickets collection
+        Ticket.find(function(error, tickets){
+            if(error){
+                console.log(error);
+                res.end(error);
+            }
+            else {
+                //no error, list of tickets
+                res.render('tickets/index',{
+                    title: 'Tickets',
+                    tickets : tickets,
+                    typeU: typeUser,
+                    displayName: req.user ? req.user.displayName : ''
+                });
+            }
+        });      
+    }
+    else{
+        res.redirect('/users');
+    }
+});
+
+// get add page
+router.get('/add', requireAuth,(req: express.Request, res: express.Response, next: any)=> {
+    res.render('tickets/add', {
+        title: 'Create New Ticket',
+        displayName: req.user ? req.user.displayName : ''
+    });
+});
+
+// POST add page - save the new ticket
+router.post('/add', requireAuth, (req: express.Request, res: express.Response, next: any) => { 
+    Ticket.create({     
+        ticketTitle: req.body.ticketTitle,
+        ticketDescription: req.body.ticketDescription,
+        ticketPriority: req.body.ticketPriority,       
+        customerName: req.body.customerName,
+        customerPhone: req.body.customerPhone,
+        incidentNarrative : 
+            {comment : 'Ticket submitted'}        
+    }, function(error, Ticket) {
+        // did we get back an error or valid Ticket object?
+        if (error) {
+            console.log(error);
+            res.end(error);
+        }
+        else {
+            res.redirect('/tickets');
+        }
+    })
+});
+// GET edit page - show the current ticket in the form
+router.get('/:id', requireAuth, (req: express.Request, res: express.Response, next: any) => {
+    var id = req.params.id;
+    Ticket.findById(id, (error, Ticket) => {
+        if (error) {
+            console.log(error);
+            res.end(error);
+        }
+        else {
+            //show the edit view
+            res.render('tickets/edit', {
+                title: 'Ticket Details',
+                ticket: Ticket,
+                displayName: req.user ? req.user.displayName : ''
+            });
+        }
+    });
+});
+// POST edit page - update the selected ticket
+router.post('/:id', requireAuth,(req: express.Request, res: express.Response, next: any) => {
+    // grab the id from the url parameter
+    var id = req.params.id;
+    // create and populate a ticket object
+    var ticket = new Ticket({
+        _id: id,
+        ticketTitle: req.body.ticketTitle,
+        ticketDescription: req.body.ticketDescription,
+        ticketPriority: req.body.ticketPriority,
+        customerName: req.body.customerName,
+        customerPhone: req.body.customerPhone,
+        ticketStatus: req.body.ticketStatus,
+        incidentNarrative : [{
+            comment: req.body.comment
+        }],
+    });
+    var incidentComment = req.body.comment;
+    var incidentStatus = req.body.ticketStatus;
+    // run the update using mongoose and our model
+    Ticket.update({ _id: id }, {$push: {incidentNarrative:{comment : incidentComment, ticketStatus :incidentStatus}}}, (error) => {
+        if (error) {
+            console.log(error);
+            res.end(error);
+        }
+        else {
+            //if success update
+            res.redirect('/tickets');
+        }
+    });
+});
+// make this public
+module.exports = router;
